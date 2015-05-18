109933deec558193fe99dcddbfb7d4317d4311cd
Gabor Gevay <ggab90@gmail.com>
2015-05-18 20:49:20 +0200

    [streaming] Fast calculation of medians of windows

---
+++ flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/windowing/helper/fieldaccessors/FieldAccessor.java
@@ -0,0 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.api.windowing.helper.fieldaccessors;
+
+public interface FieldAccessor<R, F> {
+
+	F get(R record);
+
+	// This has to return the result, because the SimpleFieldAccessor might not be able to modify the record in place.
+	// (for example, when R is simply Double)
+	R set(R record, F fieldValue);
+}
