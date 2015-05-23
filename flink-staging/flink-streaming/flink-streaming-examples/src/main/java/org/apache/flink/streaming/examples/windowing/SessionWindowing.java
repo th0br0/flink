e3f0a5773fbaaa5b31786800caad79579fb2cb57
Gabor Gevay <ggab90@gmail.com>
2015-05-23 14:32:48 +0200

    kiserletezes

--- flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/windowing/SessionWindowing.java
+++ flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/windowing/SessionWindowing.java
@@ -37,48 +37,93 @@ public class SessionWindowing {
 		}
 
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		env.setParallelism(2);
+		//env.setParallelism(2);
+		env.setParallelism(1);
 
-		final List<Tuple3<String, Long, Integer>> input = new ArrayList<Tuple3<String, Long, Integer>>();
+		final List<Tuple3<Long, Long, Integer>> input = new ArrayList<Tuple3<Long, Long, Integer>>();
 
-		input.add(new Tuple3<String, Long, Integer>("a", 1L, 1));
-		input.add(new Tuple3<String, Long, Integer>("b", 1L, 1));
-		input.add(new Tuple3<String, Long, Integer>("b", 3L, 1));
-		input.add(new Tuple3<String, Long, Integer>("b", 5L, 1));
-		input.add(new Tuple3<String, Long, Integer>("c", 6L, 1));
-		// We expect to detect the session "a" earlier than this point (the old
-		// functionality can only detect here when the next starts)
-		input.add(new Tuple3<String, Long, Integer>("a", 10L, 1));
-		// We expect to detect session "b" and "c" at this point as well
-		input.add(new Tuple3<String, Long, Integer>("c", 11L, 1));
+//		input.add(new Tuple3<Long, Long, Integer>(0L, 1L, 1));
+//		input.add(new Tuple3<Long, Long, Integer>(1L, 1L, 1));
+//		input.add(new Tuple3<Long, Long, Integer>(1L, 3L, 1));
+//		input.add(new Tuple3<Long, Long, Integer>(1L, 5L, 1));
+//		input.add(new Tuple3<Long, Long, Integer>(2L, 6L, 1));
+//		// We expect to detect the session "a" earlier than this point (the old
+//		// functionality can only detect here when the next starts)
+//		input.add(new Tuple3<Long, Long, Integer>(0L, 10L, 1));
+//		// We expect to detect session "b" and "c" at this point as well
+//		input.add(new Tuple3<Long, Long, Integer>(2L, 11L, 1));
 
-		DataStream<Tuple3<String, Long, Integer>> source = env
-				.addSource(new SourceFunction<Tuple3<String, Long, Integer>>() {
+		input.add(new Tuple3<Long, Long, Integer>(0L, 0L, 1));
+
+//<<<<<<< HEAD
+		DataStream<Tuple3<Long, Long, Integer>> source = env
+				.addSource(new SourceFunction<Tuple3<Long, Long, Integer>>() {
 					private static final long serialVersionUID = 1L;
 
 					@Override
-					public void run(SourceContext<Tuple3<String, Long, Integer>> ctx) throws Exception {
-						for (Tuple3<String, Long, Integer> value : input) {
-							// We sleep three seconds between every output so we
-							// can see whether we properly detect sessions
-							// before the next start for a specific id
-							ctx.collect(value);
-							if (!fileOutput) {
-								System.out.println("Collected: " + value);
-								Thread.sleep(3000);
+					public void run(SourceContext<Tuple3<Long, Long, Integer>> ctx) throws Exception {
+						while(true) {
+							for (Tuple3<Long, Long, Integer> value : input) {
+								// We sleep three seconds between every output so we
+								// can see whether we properly detect sessions
+								// before the next start for a specific id
+								ctx.collect(value);
+								if (!fileOutput) {
+									System.out.println("Collected: " + value);
+									//Thread.sleep(3000);
+								}
+							}
+							for (int i = 0; i < input.size(); i++) {
+								input.get(i).f0 += 3;
+								input.get(i).f1 += 11;
 							}
 						}
 					}
 
 					@Override
 					public void cancel() {
+//=======
+//		DataStream<Tuple3<Long, Long, Integer>> source = env
+//				.addSource(new SourceFunction<Tuple3<Long, Long, Integer>>() {
+//					int index = 0;
+//					int c = 0;
+//
+//					@Override
+//					public boolean reachedEnd() throws Exception {
+//						//return index >= input.size();
+//						//return index == input.size() && c == 0;
+//						//return index == input.size() && c == 100;
+//						return false;
+//					}
+//
+//					@Override
+//					public Tuple3<Long, Long, Integer> next() throws Exception {
+//						if(index == input.size()) {
+//							c++;
+//							index = 0;
+//							for (int i = 0; i < input.size(); i++) {
+//								input.get(i).f0 += 3;
+//								input.get(i).f1 += 11;
+//							}
+//						}
+//
+//						Tuple3<Long, Long, Integer> result = input.get(index);
+//						index++;
+//
+//						if (!fileOutput) {
+//							System.out.println("Collected: " + result);
+//							//Thread.sleep(3000);
+//						}
+//						//return result;
+//						return result.copy();
+//>>>>>>> kiserletezes
 					}
 				});
 
 		// We create sessions for each id with max timeout of 3 time units
-		DataStream<Tuple3<String, Long, Integer>> aggregated = source.groupBy(0)
+		DataStream<Tuple3<Long, Long, Integer>> aggregated = source.groupBy(0)
 				.window(new SessionTriggerPolicy(3L),
-						new TumblingEvictionPolicy<Tuple3<String, Long, Integer>>()).sum(2)
+						new TumblingEvictionPolicy<Tuple3<Long, Long, Integer>>()).sum(2)
 				.flatten();
 
 		if (fileOutput) {
@@ -91,12 +136,15 @@ public class SessionWindowing {
 	}
 
 	private static class SessionTriggerPolicy implements
-			CentralActiveTrigger<Tuple3<String, Long, Integer>> {
+			CentralActiveTrigger<Tuple3<Long, Long, Integer>> {
 
 		private static final long serialVersionUID = 1L;
 
 		private volatile Long lastSeenEvent = 1L;
 		private Long sessionTimeout;
+		private boolean sessionEnded = false;
+
+		static int c1 = 0, c2 = 0;
 
 		public SessionTriggerPolicy(Long sessionTimeout) {
 			this.sessionTimeout = sessionTimeout;
@@ -104,7 +152,8 @@ public class SessionWindowing {
 		}
 
 		@Override
-		public boolean notifyTrigger(Tuple3<String, Long, Integer> datapoint) {
+		public boolean notifyTrigger(Tuple3<Long, Long, Integer> datapoint) {
+			c1++;
 
 			Long eventTimestamp = datapoint.f1;
 			Long timeSinceLastEvent = eventTimestamp - lastSeenEvent;
@@ -113,6 +162,7 @@ public class SessionWindowing {
 			lastSeenEvent = eventTimestamp;
 
 			if (timeSinceLastEvent > sessionTimeout) {
+				sessionEnded = true;
 				return true;
 			} else {
 				return false;
@@ -120,7 +170,13 @@ public class SessionWindowing {
 		}
 
 		@Override
-		public Object[] notifyOnLastGlobalElement(Tuple3<String, Long, Integer> datapoint) {
+		public Object[] notifyOnLastGlobalElement(Tuple3<Long, Long, Integer> datapoint) {
+			c2++;
+
+			if(sessionEnded) {
+				return null;
+			}
+
 			Long eventTimestamp = datapoint.f1;
 			Long timeSinceLastEvent = eventTimestamp - lastSeenEvent;
 
@@ -128,6 +184,7 @@ public class SessionWindowing {
 			// belongs to a different group
 
 			if (timeSinceLastEvent > sessionTimeout) {
+				sessionEnded = true;
 				return new Object[]{datapoint};
 			} else {
 				return null;
