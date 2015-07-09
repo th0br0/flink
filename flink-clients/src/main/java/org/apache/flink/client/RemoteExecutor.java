twalthr/incubator-flink

[FLINK-1789] [core] [runtime] [java-api] Allow adding of URLs to the usercode class loader

flink-clients/src/main/java/org/apache/flink/client/RemoteExecutor.java
@@ -53,32 +53,34 @@ public class RemoteExecutor extends PlanExecutor {
 	private static final Logger LOG = LoggerFactory.getLogger(RemoteExecutor.class);
 
 	private final List<String> jarFiles;
+	private final List<String> globalClasspaths;
 	private final InetSocketAddress address;
 	
 	public RemoteExecutor(String hostname, int port) {
-		this(hostname, port, Collections.<String>emptyList());
+		this(hostname, port, Collections.<String>emptyList(), Collections.<String>emptyList());
 	}
 	
 	public RemoteExecutor(String hostname, int port, String jarFile) {
-		this(hostname, port, Collections.singletonList(jarFile));
+		this(hostname, port, Collections.singletonList(jarFile), Collections.<String>emptyList());
 	}
 	
 	public RemoteExecutor(String hostport, String jarFile) {
-		this(getInetFromHostport(hostport), Collections.singletonList(jarFile));
+		this(getInetFromHostport(hostport), Collections.singletonList(jarFile), Collections.<String>emptyList());
 	}
 	
-	public RemoteExecutor(String hostname, int port, List<String> jarFiles) {
-		this(new InetSocketAddress(hostname, port), jarFiles);
+	public RemoteExecutor(String hostname, int port, List<String> jarFiles, List<String> globalClasspaths) {
+		this(new InetSocketAddress(hostname, port), jarFiles, globalClasspaths);
 	}
 
-	public RemoteExecutor(InetSocketAddress inet, List<String> jarFiles) {
+	public RemoteExecutor(InetSocketAddress inet, List<String> jarFiles, List<String> globalClasspaths) {
 		this.jarFiles = jarFiles;
 		this.address = inet;
+		this.globalClasspaths = globalClasspaths;
 	}
 
 	@Override
 	public JobExecutionResult executePlan(Plan plan) throws Exception {
-		JobWithJars p = new JobWithJars(plan, this.jarFiles);
+		JobWithJars p = new JobWithJars(plan, this.jarFiles, this.globalClasspaths);
 		return executePlanWithJars(p);
 	}
 	
@@ -113,7 +115,7 @@ public class RemoteExecutor extends PlanExecutor {
 
 	@Override
 	public String getOptimizerPlanAsJSON(Plan plan) throws Exception {
-		JobWithJars p = new JobWithJars(plan, this.jarFiles);
+		JobWithJars p = new JobWithJars(plan, this.jarFiles, this.globalClasspaths);
 		Client c = new Client(this.address, new Configuration(), p.getUserCodeClassLoader(), -1);
 		
 		OptimizedPlan op = (OptimizedPlan) c.getOptimizedPlan(p, -1);

flink-java/src/main/java/org/apache/flink/api/java/ExecutionEnvironment.java
@@ -942,7 +942,7 @@ public abstract class ExecutionEnvironment {
 				}
 				if(typeInfo instanceof CompositeType) {
 					List<GenericTypeInfo<?>> genericTypesInComposite = new ArrayList<GenericTypeInfo<?>>();
-					Utils.getContainedGenericTypes((CompositeType)typeInfo, genericTypesInComposite);
+					Utils.getContainedGenericTypes((CompositeType<?>)typeInfo, genericTypesInComposite);
 					for(GenericTypeInfo<?> gt : genericTypesInComposite) {
 						Serializers.recursivelyRegisterType(gt.getTypeClass(), config);
 					}
@@ -1097,7 +1097,7 @@ public abstract class ExecutionEnvironment {
 	 * @return A remote environment that executes the program on a cluster.
 	 */
 	public static ExecutionEnvironment createRemoteEnvironment(String host, int port, String... jarFiles) {
-		return new RemoteEnvironment(host, port, jarFiles);
+		return new RemoteEnvironment(host, port, jarFiles, null);
 	}
 
 	/**
@@ -1114,7 +1114,30 @@ public abstract class ExecutionEnvironment {
 	 * @return A remote environment that executes the program on a cluster.
 	 */
 	public static ExecutionEnvironment createRemoteEnvironment(String host, int port, int parallelism, String... jarFiles) {
-		RemoteEnvironment rec = new RemoteEnvironment(host, port, jarFiles);
+		RemoteEnvironment rec = new RemoteEnvironment(host, port, jarFiles, null);
+		rec.setParallelism(parallelism);
+		return rec;
+	}
+	
+	/**
+	 * Creates a {@link RemoteEnvironment}. The remote environment sends (parts of) the program 
+	 * to a cluster for execution. Note that all file paths used in the program must be accessible from the
+	 * cluster. The execution will use the specified parallelism.
+	 * 
+	 * @param host The host name or address of the master (JobManager), where the program should be executed.
+	 * @param port The port of the master (JobManager), where the program should be executed.
+	 * @param parallelism The parallelism to use during the execution.
+	 * @param jarFiles The JAR files with code that needs to be shipped to the cluster. If the program uses
+	 *                 user-defined functions, user-defined input formats, or any libraries, those must be
+	 *                 provided in the JAR files.
+	 * @param globalClasspaths The paths of directories and JAR files that are added to each user code 
+	 *                 classloader on all nodes in the cluster. Note that the paths must specify a
+	 *                 protocol (e.g. file://) and be accessible on all nodes (e.g. by means of a NFS share).
+	 * @return A remote environment that executes the program on a cluster.
+	 */
+	public static ExecutionEnvironment createRemoteEnvironment(String host, int port, int parallelism,
+			String[] jarFiles, String[] globalClasspaths) {
+		RemoteEnvironment rec = new RemoteEnvironment(host, port, jarFiles, globalClasspaths);
 		rec.setParallelism(parallelism);
 		return rec;
 	}

th0br0/flink

flink-clients/src/main/java/org/apache/flink/client/RemoteExecutor.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
@@ -52,33 +53,35 @@ public class RemoteExecutor extends PlanExecutor {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(RemoteExecutor.class);
 
-	private final List<String> jarFiles;
+	private final List<URL> jarFiles;
+	private final List<URL> globalClasspaths;
 	private final InetSocketAddress address;
 	
 	public RemoteExecutor(String hostname, int port) {
-		this(hostname, port, Collections.<String>emptyList());
+		this(hostname, port, Collections.<URL>emptyList(), Collections.<URL>emptyList());
 	}
 	
-	public RemoteExecutor(String hostname, int port, String jarFile) {
-		this(hostname, port, Collections.singletonList(jarFile));
+	public RemoteExecutor(String hostname, int port, URL jarFile) {
+		this(hostname, port, Collections.singletonList(jarFile), Collections.<URL>emptyList());
 	}
 	
-	public RemoteExecutor(String hostport, String jarFile) {
-		this(getInetFromHostport(hostport), Collections.singletonList(jarFile));
+	public RemoteExecutor(String hostport, URL jarFile) {
+		this(getInetFromHostport(hostport), Collections.singletonList(jarFile), Collections.<URL>emptyList());
 	}
 	
-	public RemoteExecutor(String hostname, int port, List<String> jarFiles) {
-		this(new InetSocketAddress(hostname, port), jarFiles);
+	public RemoteExecutor(String hostname, int port, List<URL> jarFiles, List<URL> globalClasspaths) {
+		this(new InetSocketAddress(hostname, port), jarFiles, globalClasspaths);
 	}
 
-	public RemoteExecutor(InetSocketAddress inet, List<String> jarFiles) {
+	public RemoteExecutor(InetSocketAddress inet, List<URL> jarFiles, List<URL> globalClasspaths) {
 		this.jarFiles = jarFiles;
 		this.address = inet;
+		this.globalClasspaths = globalClasspaths;
 	}
 
 	@Override
 	public JobExecutionResult executePlan(Plan plan) throws Exception {
-		JobWithJars p = new JobWithJars(plan, this.jarFiles);
+		JobWithJars p = new JobWithJars(plan, this.jarFiles, this.globalClasspaths);
 		return executePlanWithJars(p);
 	}
 	
@@ -113,7 +116,7 @@ public class RemoteExecutor extends PlanExecutor {
 
 	@Override
 	public String getOptimizerPlanAsJSON(Plan plan) throws Exception {
-		JobWithJars p = new JobWithJars(plan, this.jarFiles);
+		JobWithJars p = new JobWithJars(plan, this.jarFiles, this.globalClasspaths);
 		Client c = new Client(this.address, new Configuration(), p.getUserCodeClassLoader(), -1);
 		
 		OptimizedPlan op = (OptimizedPlan) c.getOptimizedPlan(p, -1);

flink-java/src/main/java/org/apache/flink/api/java/ExecutionEnvironment.java
@@ -19,6 +19,8 @@
 package org.apache.flink.api.java;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -942,7 +944,7 @@ public abstract class ExecutionEnvironment {
 				}
 				if(typeInfo instanceof CompositeType) {
 					List<GenericTypeInfo<?>> genericTypesInComposite = new ArrayList<GenericTypeInfo<?>>();
-					Utils.getContainedGenericTypes((CompositeType)typeInfo, genericTypesInComposite);
+					Utils.getContainedGenericTypes((CompositeType<?>)typeInfo, genericTypesInComposite);
 					for(GenericTypeInfo<?> gt : genericTypesInComposite) {
 						Serializers.recursivelyRegisterType(gt.getTypeClass(), config);
 					}
@@ -1101,7 +1103,7 @@ public abstract class ExecutionEnvironment {
 	 * @return A remote environment that executes the program on a cluster.
 	 */
 	public static ExecutionEnvironment createRemoteEnvironment(String host, int port, String... jarFiles) {
-		return new RemoteEnvironment(host, port, jarFiles);
+		return new RemoteEnvironment(host, port, jarFiles, null);
 	}
 
 	/**
@@ -1118,7 +1120,30 @@ public abstract class ExecutionEnvironment {
 	 * @return A remote environment that executes the program on a cluster.
 	 */
 	public static ExecutionEnvironment createRemoteEnvironment(String host, int port, int parallelism, String... jarFiles) {
-		RemoteEnvironment rec = new RemoteEnvironment(host, port, jarFiles);
+		RemoteEnvironment rec = new RemoteEnvironment(host, port, jarFiles, null);
+		rec.setParallelism(parallelism);
+		return rec;
+	}
+	
+	/**
+	 * Creates a {@link RemoteEnvironment}. The remote environment sends (parts of) the program 
+	 * to a cluster for execution. Note that all file paths used in the program must be accessible from the
+	 * cluster. The execution will use the specified parallelism.
+	 * 
+	 * @param host The host name or address of the master (JobManager), where the program should be executed.
+	 * @param port The port of the master (JobManager), where the program should be executed.
+	 * @param parallelism The parallelism to use during the execution.
+	 * @param jarFiles The JAR files with code that needs to be shipped to the cluster. If the program uses
+	 *                 user-defined functions, user-defined input formats, or any libraries, those must be
+	 *                 provided in the JAR files.
+	 * @param globalClasspaths The URLs of directories and JAR files that are added to each user code 
+	 *                 classloader on all nodes in the cluster. Note that the paths must specify a
+	 *                 protocol (e.g. file://) and be accessible on all nodes (e.g. by means of a NFS share).
+	 * @return A remote environment that executes the program on a cluster.
+	 */
+	public static ExecutionEnvironment createRemoteEnvironment(String host, int port, int parallelism,
+			String[] jarFiles, URL[] globalClasspaths) {
+		RemoteEnvironment rec = new RemoteEnvironment(host, port, jarFiles, globalClasspaths);
 		rec.setParallelism(parallelism);
 		return rec;
 	}