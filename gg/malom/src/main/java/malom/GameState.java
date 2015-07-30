811d3d774e8a563ee47bc6749dccbc846ad68c51
Gabor Gevay <ggab90@gmail.com>
2015-07-30 19:31:52 +0200

    [Malom] Add toString, compareTo, equals, default constructors, etc. to various classes
    and lots of temporary debugging code

--- gg/malom/src/main/java/malom/GameState.java
+++ gg/malom/src/main/java/malom/GameState.java
@@ -9,11 +9,28 @@ public class GameState implements Comparable<GameState> {
 		this.board = board;
 	}
 
+	public GameState() {}
+
 	@Override
 	public int compareTo(GameState o) {
 		if(!sid.equals(o.sid))
 			return sid.compareTo(o.sid);
 		else
 			return ((Long)board).compareTo(o.board);
+	}
+
+	@Override
+	public boolean equals(Object o0) {
+		if(o0 instanceof GameState) {
+			GameState o = (GameState)o0;
+			return sid.equals(o.sid) && board == o.board;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return sid.toString() + " " + Long.toBinaryString(board);
 	}
 }
