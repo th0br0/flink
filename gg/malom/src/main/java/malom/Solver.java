98d9af582453197226178c5da0cb24e8cfdb99a7
Gabor Gevay <ggab90@gmail.com>
2015-08-12 18:34:11 +0200

    [Malom] wip2

--- gg/malom/src/main/java/malom/Solver.java
+++ gg/malom/src/main/java/malom/Solver.java
@@ -7,9 +7,13 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.Vertex;
 import org.apache.flink.types.NullValue;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Solver {
 
@@ -34,7 +38,21 @@ public class Solver {
 		Graph<GameState, ValueCount, NullValue> res = retr.run();
 		//res.getVertices().print();
 		//System.out.println(res.getVertices().count());
+
+
 		res.getVertices().writeAsText("/home/gabor/tmp/res.txt", FileSystem.WriteMode.OVERWRITE);
 		env.execute();
+
+
+//		List<Vertex<GameState, ValueCount>> resList = res.getVertices().collect();
+//		Map<Long, ValueCount> resMap = new TreeMap<>();
+//		for(Vertex<GameState, ValueCount> v: resList) {
+//			resMap.put(v.getId().board, v.getValue());
+//		}
+//		for(Vertex<GameState, ValueCount> v: resList) {
+//			if(resMap.get(Symmetries.minSym48(v.getId().board)).value != v.getValue().value) {
+//				int a = 42;
+//			}
+//		}
 	}
 }
