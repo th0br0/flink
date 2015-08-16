20ecc6d1cb4077cf5451c5a84779a1af17379e1a
Gabor Gevay <ggab90@gmail.com>
2015-08-16 14:37:15 +0200

    [Malom] VertexUpdateFunction calls setNewVertexValue only once

--- gg/malom/src/main/java/malom/Config.java
+++ gg/malom/src/main/java/malom/Config.java
@@ -1,12 +1,10 @@
 package malom;
 
-import org.apache.log4j.PropertyConfigurator;
-
 public class Config {
 	static String movegen_file = "/tmp/movegen";
 
 	static final int max_ksz = 9;
 	static final String variantName = "std";
 
-	static final boolean filterSym = false;
+	static final boolean filterSym = true;
 }
