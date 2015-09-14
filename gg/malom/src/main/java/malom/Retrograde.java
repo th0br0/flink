5a8235212381c7a45c6c070a65d1b3709d55420b
Gabor Gevay <ggab90@gmail.com>
2015-09-14 15:20:18 +0200

    [Malom] Refactor: moved SectorGraph.solve into Retrograde

--- gg/malom/src/main/java/malom/Retrograde.java
+++ gg/malom/src/main/java/malom/Retrograde.java
@@ -1,13 +1,14 @@
 package malom;
 
 
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.operators.base.JoinOperatorBase;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.graph.Graph;
 import org.apache.flink.graph.Vertex;
 import org.apache.flink.graph.Edge;
@@ -19,16 +20,104 @@ import org.apache.flink.types.NullValue;
 import org.apache.flink.util.Collector;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Retrograde implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	public static DataSet<Vertex<GameState, ValueCount>> createSectorVertices(SectorId s, ValueCount vv, ExecutionEnvironment env) {
+	ExecutionEnvironment env;
+
+	private Map<SectorId, DataSet<Vertex<GameState, ValueCount>>> results = new HashMap<>();
+
+
+	public Retrograde(ExecutionEnvironment env) {
+		this.env = env;
+	}
+
+	/**
+	 * Compute the game-theoretical solution for all states of the work unit of the given sector.
+	 * Recursively calls itself for sectors that the work unit directly depends on, and uses memoization (the results map).
+	 * (Note: we don't actually compute anything here when the method is called; we are just creating Flink DataSets.)
+	 *
+	 * The computation will go one work unit at a time. A work unit is either one non-twin sector (see sectorId.hasTwin), or
+	 * two sectors that are twins of each other. (Assuming a more general sector graph, the work units should be the
+	 * strongly connected components. This is the case here: the only loops are self-edges and back-and-forth edges between
+	 * twin nodes.)
+	 */
+	public DataSet<Vertex<GameState, ValueCount>> solve(SectorId u) {
+		if(u.isLosing()) {
+			results.put(u, createSectorVertices(u, ValueCount.value(Value.loss(0))));
+		} else {
+
+			// The main sectors are the one or two sectors of the work unit.
+			// The child sectors are those sectors, that the main sectors directly depend on.
+			// The sector family is all these together.
+
+			Set<SectorId> mainSectors = new HashSet<>(Arrays.asList(u, u.negate()));
+			Set<SectorId> chdSectors = new HashSet<>();
+			for(SectorId mainSec: mainSectors) {
+				for(SectorId chdSec: SectorGraph.graphFunc(mainSec)) {
+					if(!mainSectors.contains(chdSec)) {
+						chdSectors.add(chdSec);
+					}
+				}
+			}
+			assert chdSectors.size() > 0;
+			List<SectorId> sectorFamily = new ArrayList<>();
+			sectorFamily.addAll(mainSectors);
+			sectorFamily.addAll(chdSectors);
+
+			// The vertices DataSet will consist of all the vertices of the sector family.
+			// - vertices of child sectors will have been already solved
+			// - vertices of main sectors will be initialized to undefined
+
+			DataSet<Vertex<GameState, ValueCount>> vertices = null;
+
+			for(SectorId chdSector: chdSectors) {
+				DataSet<Vertex<GameState, ValueCount>> currentSectorVertices = solve(chdSector);
+				if(vertices == null) {
+					vertices = currentSectorVertices;
+				} else {
+					vertices = vertices.union(currentSectorVertices);
+				}
+			}
+
+			// Add the main sectors
+			vertices = vertices.union(createSectorVertices(u, ValueCount.count(-1)));
+			if(u.hasTwin()) {
+				vertices = vertices.union(createSectorVertices(u.negate(), ValueCount.count(-1)));
+			}
+
+			// Create the edges
+			DataSet<Edge<GameState, NullValue>> edges = createEdges(vertices, sectorFamily, mainSectors);
+
+			Graph<GameState, ValueCount, NullValue> g = Graph.fromDataSet(vertices, edges, env);
+
+			// Initialize main sectors (we do this here, because we need the degrees)
+			g = init(g, u, u.hasTwin() ? u.negate() : null, env);
+
+			// The essence of the computation
+			g = Retrograde.iterate(g);
+
+			// We need a result DataSet for each of the main sectors.
+			results.put(u, g.getVertices().filter(new FilterToOneSector(u)));
+			if(u.hasTwin()) {
+				results.put(u.negate(), g.getVertices().filter(new FilterToOneSector(u.negate())));
+			}
+
+			// Write the result files
+			results.get(u).writeAsText(Config.resultOutPath(u), FileSystem.WriteMode.OVERWRITE);
+			if(u.hasTwin()) {
+				results.get(u.negate()).writeAsText(Config.resultOutPath(u.negate()), FileSystem.WriteMode.OVERWRITE);
+			}
+
+			// Compute verify and write it
+			Verify.verify(g, u, u.hasTwin() ? u.negate() : null);
+		}
+		return results.get(u);
+	}
+
+	public DataSet<Vertex<GameState, ValueCount>> createSectorVertices(SectorId s, ValueCount vv) {
 		DataSet<GameState> gameStates = env.fromCollection(new SectorElementIterator(s), GameState.class);
 		if(Config.filterSym) {
 			gameStates = gameStates.filter(state -> !Symmetries.isFiltered(state.board));
@@ -41,7 +130,7 @@ public class Retrograde implements Serializable {
 		}).name("Vertices " + s);
 	}
 
-	public static DataSet<Edge<GameState, NullValue>> createEdges(DataSet<Vertex<GameState, ValueCount>> vertices, Collection<SectorId> sectors, Collection<SectorId> mainSectors) {
+	public DataSet<Edge<GameState, NullValue>> createEdges(DataSet<Vertex<GameState, ValueCount>> vertices, Collection<SectorId> sectors, Collection<SectorId> mainSectors) {
 		Movegen movegen = new Movegen(sectors, mainSectors);
 		return vertices.flatMap(new FlatMapFunction<Vertex<GameState, ValueCount>, Edge<GameState, NullValue>>() {
 			@Override
@@ -78,7 +167,7 @@ public class Retrograde implements Serializable {
 	 *  @param mainSec2 The other main sector (null, if not twin)
 	 *  @return		g transformed, by initializing the vertices of the main sectors.
  	 */
-	public static Graph<GameState, ValueCount, NullValue> init(
+	public Graph<GameState, ValueCount, NullValue> init(
 			Graph<GameState, ValueCount, NullValue> g,
 			SectorId mainSec1, SectorId mainSec2,
 			ExecutionEnvironment env) {
@@ -159,7 +248,7 @@ public class Retrograde implements Serializable {
 						assert vv.value.depth <= msg.depth + 1;
 					}
 				}
-				if(newValueSet) {
+				if (newValueSet) {
 					setNewVertexValue(vv);
 				}
 			}
@@ -174,5 +263,20 @@ public class Retrograde implements Serializable {
 				}
 			}
 		}, 1000, config);
+	}
+
+
+	public class FilterToOneSector implements FilterFunction<Vertex<GameState, ValueCount>> {
+
+		SectorId s;
+
+		public FilterToOneSector(SectorId s) {
+			this.s = s;
+		}
+
+		@Override
+		public boolean filter(Vertex<GameState, ValueCount> value) throws Exception {
+			return value.getId().sid.equals(s);
+		}
 	}
 }
--- gg/malom/src/main/java/malom/SectorGraph.java
+++ gg/malom/src/main/java/malom/SectorGraph.java
@@ -1,138 +1,17 @@
 package malom;
 
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.core.fs.FileSystem;
-import org.apache.flink.graph.Edge;
-import org.apache.flink.graph.Graph;
-import org.apache.flink.graph.Vertex;
-import org.apache.flink.types.NullValue;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
- * This class represents the subdivision of the state space into sectors, and drives the computation,
- * by calling into the Retrograde class.
+ * This class represents the subdivision of the state space into sectors.
  * The concept of "sector graph" means a graph where the vertices are the sectors, and there is a directed edge from
  * u sector to v sector, if there is a game state in u from where you can make a movoe to a game state in v.
- *
- * The computation will go one work unit at a time. A work unit is either one non-twin sector (see sectorId.hasTwin), or
- * two sectors that are twins of each other. (Assuming a more general sector graph, the work units should be the
- * strongly connected components. This is the case here: the only loops are self-edges and back-and-forth edges between
- * twin nodes.).
  */
 public class SectorGraph {
 
-	ExecutionEnvironment env;
-
-	private Map<SectorId, DataSet<Vertex<GameState, ValueCount>>> results = new HashMap<>();
-
-	public SectorGraph(ExecutionEnvironment env) {
-		this.env = env;
-	}
-
-	/**
-	 * Compute the game-theoretical solution for all states of the work unit of the given sector.
-	 * Recursively calls itself for sectors that the work unit directly depends on, and uses memoization (the results map).
-	 * (Note: we don't actually compute anything here when the method is called; we are just creating Flink DataSets.)
-	 */
-	public DataSet<Vertex<GameState, ValueCount>> solve(SectorId u) {
-		if(u.isLosing()) {
-			results.put(u, Retrograde.createSectorVertices(u, ValueCount.value(Value.loss(0)), env));
-		} else {
-
-			// The main sectors are the one or two sectors of the work unit.
-			// The child sectors are those sectors, that the main sectors directly depend on.
-			// The sector family is all these together.
-
-			Set<SectorId> mainSectors = new HashSet<>(Arrays.asList(u, u.negate()));
-			Set<SectorId> chdSectors = new HashSet<>();
-			for(SectorId mainSec: mainSectors) {
-				chdSectors.addAll(graphFunc(mainSec).
-						stream().filter(chdSec -> !mainSectors.contains(chdSec)).
-						collect(Collectors.toList()));
-			}
-			assert chdSectors.size() > 0;
-			List<SectorId> sectorFamily = new ArrayList<>();
-			sectorFamily.addAll(mainSectors);
-			sectorFamily.addAll(chdSectors);
-
-			// The vertices DataSet will consist of all the vertices of the sector family.
-			// - vertices of child sectors will have been already solved
-			// - vertices of main sectors will be initialized to undefined
-
-			DataSet<Vertex<GameState, ValueCount>> vertices = null;
-
-			for(SectorId chdSector: chdSectors) {
-				DataSet<Vertex<GameState, ValueCount>> currentSectorVertices = solve(chdSector);
-				if(vertices == null) {
-					vertices = currentSectorVertices;
-				} else {
-					vertices = vertices.union(currentSectorVertices);
-				}
-			}
-
-			// Add the main sectors
-			vertices = vertices.union(Retrograde.createSectorVertices(u, ValueCount.count(-1), env));
-			if(u.hasTwin()) {
-				vertices = vertices.union(Retrograde.createSectorVertices(u.negate(), ValueCount.count(-1), env));
-			}
-
-			// Create the edges
-			DataSet<Edge<GameState, NullValue>> edges = Retrograde.createEdges(vertices, sectorFamily, mainSectors);
-
-			Graph<GameState, ValueCount, NullValue> g = Graph.fromDataSet(vertices, edges, env);
-
-			// Initialize main sectors (we do this here, because we need the degrees)
-			g = Retrograde.init(g, u, u.hasTwin() ? u.negate() : null, env);
-
-			// The essence of the computation
-			g = Retrograde.iterate(g);
-
-			// We need a result DataSet for each of the main sectors.
-			results.put(u, g.getVertices().filter(new FilterToOneSector(u)));
-			if(u.hasTwin()) {
-				results.put(u.negate(), g.getVertices().filter(new FilterToOneSector(u.negate())));
-			}
-
-			// Write the result files
-			results.get(u).writeAsText(Config.resultOutPath(u), FileSystem.WriteMode.OVERWRITE);
-			if(u.hasTwin()) {
-				results.get(u.negate()).writeAsText(Config.resultOutPath(u.negate()), FileSystem.WriteMode.OVERWRITE);
-			}
-
-			// Compute verify and write it
-			Verify.verify(g, u, u.hasTwin() ? u.negate() : null);
-		}
-		return results.get(u);
-	}
-
-
-	public static class FilterToOneSector implements FilterFunction<Vertex<GameState, ValueCount>> {
-
-		SectorId s;
-
-		public FilterToOneSector(SectorId s) {
-			this.s = s;
-		}
-
-		@Override
-		public boolean filter(Vertex<GameState, ValueCount> value) throws Exception {
-			return value.getId().sid.equals(s);
-		}
-	}
-
-
 	// Returns the outgoing edges from a sector
-	private List<SectorId> graphFunc(SectorId u) {
+	public static List<SectorId> graphFunc(SectorId u) {
 		List<SectorId> r0 = graphFunc0(u);
 		List<SectorId> r = new ArrayList<>();
 		for (SectorId x : r0) {
@@ -145,7 +24,7 @@ public class SectorGraph {
 		return r;
 	}
 
-	private List<SectorId> graphFunc0(SectorId u) {
+	private static List<SectorId> graphFunc0(SectorId u) {
 		// In the standard variant, there are two outgoing edges from each node: we either close a mill, or not.
 		List<SectorId> v = new ArrayList<>();
 		v.add(new SectorId(u));
